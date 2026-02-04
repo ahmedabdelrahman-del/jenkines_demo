f7310c5222f219f380f128d3adac45e4c012883f
Ahmed_ <ahmed.abdelrahman@sjsu.edu>
2026-02-04 12:27:45 -0800

    Delete api-gateway directory

--- api-gateway/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const userRoutes = require('./userRoutes');
-const productRoutes = require('./productRoutes');
-const orderRoutes = require('./orderRoutes');
-
-module.exports = {
-  userRoutes,
-  productRoutes,
-  orderRoutes
-};
